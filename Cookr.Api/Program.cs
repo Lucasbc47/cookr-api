115c69c23d9376b7fbdada7a769fe711c803b497
Lucas <lucasbarboza1costa@gmail.com>
2026-07-18 16:01:51 -0300

    feat: adiciona swagger e expoe versao no endpoint raiz

--- Cookr.Api/Program.cs
+++ Cookr.Api/Program.cs
@@ -16,9 +16,19 @@ builder.Services.AddDbContext<CookrDbContext>(options =>
 builder.Services.AddScoped<IRecipeService, RecipeService>();
 builder.Services.AddScoped<IIngredientService, IngredientService>();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok(new { message = "Cookr is running!" }));
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+var version = typeof(Program).Assembly.GetName().Version?.ToString(3);
+app.MapGet("/", () => Results.Ok(new { message = "Cookr is running!", version }));
 
 app.MapRecipeEndpoints();
 app.MapIngredientEndpoints();
